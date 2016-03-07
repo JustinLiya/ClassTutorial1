93888f3a8eab0f9638c894ae2aaa26daa1cfe47b
Jayashankha Liyanapathirana <Jayashankha-Liyanapa@live.nmit.ac.nz>
2016-03-07 20:13:02 +1300

    4. copleted missing photo and sculpture

--- Version 1 C/clsPhotograph.cs
+++ Version 1 C/clsPhotograph.cs
@@ -10,9 +10,19 @@ namespace Version_1_C
         private float theHeight;
         private string theType;
 
+        private static frmPhotograph photographDialog;
+
         public override void EditDetails()
         {
-  
+            if (photographDialog == null)
+            {
+                photographDialog = new frmPhotograph();
+            }
+            photographDialog.SetDetails(_Name, theDate, theValue, theWidth, theHeight, theType);
+            if (photographDialog.ShowDialog() == DialogResult.OK)
+            {
+                photographDialog.GetDetails(ref _Name, ref theDate, ref theValue, ref theWidth, ref theHeight, ref theType);
+            }
         }
     }
 }
